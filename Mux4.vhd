36db2ed9b7f773911b0799d8b45b2155ab66ec8f
Shreyan <shreyan.gupta96@gmail.com>
2016-04-02 22:40:51 +0530

    Mux4 added

--- Mux4.vhd
+++ Mux4.vhd
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date:    22:37:01 04/02/2016 
+-- Create Date:    12:34:45 03/11/2016 
 -- Design Name: 
--- Module Name:    Mux4 - Behavioral 
+-- Module Name:    Mux - Behavioral 
 -- Project Name: 
 -- Target Devices: 
 -- Tool versions: 
@@ -30,12 +30,29 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity Mux4 is
+port(
+	inval0 : in std_logic_vector(31 downto 0);
+	inval1 : in std_logic_vector(31 downto 0);
+	inval2 : in std_logic_vector(31 downto 0);
+	inval3 : in std_logic_vector(31 downto 0);
+	control : in std_logic_vector(1 downto 0);
+	outval : out std_logic_vector(31 downto 0)
+);
 end Mux4;
 
-architecture Behavioral of Mux4 is
-
+architecture Behavioral of Mux is
 begin
 
+process(inval0,inval1,inval2,inval3,control)
+begin
+	case control of
+		when "00" => outval <= inval0;
+		when "01" => outval <= inval1;
+		when "10" => outval <= inval2;
+		when "11" => outval <= inval3;
+		when others => null;
+	end case;
+end process;
 
 end Behavioral;
 
